ffe62a474cad475c67635df93296922f9a6a46eb
jayeshgupta3502 <60725831+jayeshgupta3502@users.noreply.github.com>
2020-02-06 11:02:02 +0530

    Add files via upload

---
+++ button.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import os
+import Tkinter
+import tkMessageBox
+top=Tkinter.Tk()
+
+def helloCallBack():
+    os.system('SendEmail.py')
+
+B=Tkinter.Button(top,text="hello",command= helloCallBack)
+B.pack()
+top.mainloop()
